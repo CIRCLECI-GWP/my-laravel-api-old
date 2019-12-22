236028ff21bddcb971143d155f568742dd437851
Ndagi Stanley <ndagis@gmail.com>
2019-12-22 19:03:22 +0300

    Add UserLogin test

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -37,4 +37,28 @@ class UserTest extends TestCase
             ]);
     }//testUserCreation
 
+    public function testUserLogin()
+{
+    $name = $this->faker->name();
+   	$email = $this->faker->email();
+
+    $user = new User([
+        'name' => $name,
+        'email' => $email,
+        'password' => bcrypt($this->password)
+    ]);
+
+    $user->save();
+
+    $response = $this->postJson('/api/auth/login', [
+        'email' => $email,
+        'password' => $this->password
+    ]);
+
+
+    $response->assertStatus(200);
+    $this->assertAuthenticated();
+}
+
+
 }
